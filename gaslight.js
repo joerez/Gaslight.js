3471b6b8b8a11a27d3210d8af4d47ac6c20bffde
Joerez <rezenbiz@gmail.com>
2018-09-20 16:46:02 -0700

    added more modules

--- gaslight.js
+++ gaslight.js
@@ -1,13 +1,42 @@
-gaslightExit = document.querySelector('.gaslight-exit');
+function getOffset(el) {
+  const rect = el.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY
+  };
+}
 
-gaslightExit.addEventListener("mouseover", function() {
 
-  let x = Math.random() * 300;
-  let y = Math.random() * 300;
+let neverClick = function(element, xPixels = 300, yPixels = 300, speed = 400) {
+  element = document.querySelector(element);
+  element.addEventListener("mouseover", function() {
 
-  this.style.transform = 'translateX('+ x + 'px) translateY(' + y + 'px)';
-})
+    let x = Math.random() * xPixels;
+    let y = Math.random() * yPixels;
 
-gaslightExit.addEventListener("click", function() {
-  model.style.display = 'none';
-})
+    this.style.transition = 'all ' + speed + 'ms';
+    this.style.transform = 'translateX('+ x + 'px) translateY(' + y + 'px)';
+  })
+}
+
+let swapClick = function(expectedClick, swappedClick) {
+  expectedClick = document.querySelector(expectedClick)
+  swappedClick = document.querySelector(swappedClick)
+
+
+
+  expectedClick.addEventListener("mousedown", function() {
+    let aParent = expectedClick.parentNode;
+    let bParent = swappedClick.parentNode;
+
+    let aHolder = document.createElement("div");
+    let bHolder = document.createElement("div");
+
+    aParent.replaceChild(aHolder,expectedClick);
+    bParent.replaceChild(bHolder,swappedClick);
+
+    aParent.replaceChild(swappedClick,aHolder);
+    bParent.replaceChild(expectedClick,bHolder);
+  })
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-const bringModel = document.querySelector('.bring-model');
-const model = document.querySelector('.model');
+const bringNeverClickModel = document.querySelector('.bring-neverclick-model');
+const bringSwapClickModel = document.querySelector('.bring-swapclick-model');
 
-bringModel.addEventListener("click", function() {
-  model.style.display = "flex";
+const neverClickModel = document.querySelector('.never-click-model');
+const swapClickModel = document.querySelector('.swap-click-model');
+
+const donate = document.querySelector('#donate');
+
+gaslightExit = document.querySelector('.gaslight-exit');
+submitBtn = document.querySelector('.submit-btn');
+
+bringNeverClickModel.addEventListener("click", function() {
+  neverClickModel.style.display = "flex";
 })
+
+bringSwapClickModel.addEventListener("click", function() {
+  swapClickModel.style.display = "flex";
+})
+
+
+submitBtn.addEventListener("click", function() {
+  neverClickModel.style.display = 'none';
+})
+
+donate.addEventListener("click", function() {
+  swapClickModel.style.display = 'none';
+})
+
+
+neverClick('.gaslight-exit')
+swapClick('#cancel', '#donate')
